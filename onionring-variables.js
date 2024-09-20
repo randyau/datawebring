db350c41e896c451861f4abdaee7470d4b09ebc6
Randy Au <randy.au@gmail.com>
2024-09-20 12:44:08 -0400

    Update onionring-variables.js

--- onionring-variables.js
+++ onionring-variables.js
@@ -23,7 +23,7 @@ var ringID = 'data-ring-dot-list';
 //should the widget include a link to an index page?
 var useIndex = false;
 //the full URL of the index page. if you're not using one, you don't have to specify anything here
-var indexPage = 'https://example.com/index.html';
+var indexPage = 'https://randyau.github.io/datawebring/index.html';
 
 //should the widget include a random button?
 var useRandom = true;
