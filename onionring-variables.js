e06905d6422b5129591ef46e606bf4dbb5ba9fc7
Emily Riederer <emilyriederer@gmail.com>
2024-11-13 19:09:20 -0600

    Update onionring-variables.js
    Add Emily Riederer site

--- onionring-variables.js
+++ onionring-variables.js
@@ -19,6 +19,7 @@ var sites = [
     'https://elliotgunn.github.io/',
     'https://radbrt.com',
     'https://simon.podhajsky.net/blog/',
+    'https://emilyriederer.com/'
 ];
 
 //the name of the ring
